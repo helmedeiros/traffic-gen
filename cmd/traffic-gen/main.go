cf5b4119f0dda00a95a2e9922e3279d33ddeb4c6
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2023-02-08 14:32:42 +0200

    feat(cmd): traffic-gen binary with flag-driven wiring
    cmd/traffic-gen wires the Generator + Poster into a runnable binary.
    Five flags:
    
      --target     target URL (default http://localhost:8080/decide)
      --qps        target requests/sec (default 100)
      --duration   stop after this duration (default 0 = until SIGINT)
      --seed       Generator RNG seed (default current nanos = non-deterministic)
      --timeout    per-request HTTP timeout (default 5s)
    
    main() wires SIGINT/SIGTERM into a context.CancelFunc and delegates
    to run(ctx, args, stdout, stderr) -- the same split markup-svc uses
    so tests drive the binary without spawning a process.
    
    defaultBiases() ships a v0.0.1 persona mix targeting markup-svc's
    testdata/rules.csv: customer_tier 20/30/30/20, country 30/25/20/10/10/5,
    time_window 30/40/30, channel 60/30/10. The proportions intentionally
    include slices that do NOT match any rule so the no-match path is
    exercised alongside the success path.
    
    Boot-time configuration writes to stdout (for structured-log
    capture); the per-run poster Summary writes to stderr (kept out of
    the structured-log stream).
    
    Four tests cover the operationally interesting paths: happy round-trip
    against an httptest server with Content-Type + JSON body shape
    asserted, bad-flag rejection, negative QPS rejection, ctx-cancel
    termination.
    
    Compile-time assertions pin that *randommix.Generator satisfies
    traffic.Generator and *poster.Poster satisfies traffic.Poster --
    a future port-signature change fails at build rather than at runtime.

---
+++ cmd/traffic-gen/main.go
@@ -0,0 +1,127 @@
+// Package main is the traffic-gen entry point. Boots a single
+// goroutine that draws Request shapes from the configured Generator
+// and POSTs them at the configured QPS to the target URL. See
+// traffic-gen/ADR-0001 for the wider design and the docs/cookbook/
+// for operator-side recipes.
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/helmedeiros/traffic-gen/internal/traffic"
+	"github.com/helmedeiros/traffic-gen/internal/traffic/poster"
+	"github.com/helmedeiros/traffic-gen/internal/traffic/randommix"
+)
+
+func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+	if err := run(ctx, os.Args[1:], os.Stdout, os.Stderr); err != nil {
+		fmt.Fprintf(os.Stderr, "traffic-gen: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+// run wires the binary. Separated from main so tests can drive it
+// with a cancellable ctx, captured stdout/stderr, and synthetic
+// args without spawning a real process.
+func run(ctx context.Context, args []string, stdout, stderr io.Writer) error {
+	fs := flag.NewFlagSet("traffic-gen", flag.ContinueOnError)
+	fs.SetOutput(stderr)
+	target := fs.String("target", "http://localhost:8080/decide", "target URL to POST generated Request bodies at")
+	qps := fs.Int("qps", 100, "target requests per second (paced via a 1s/QPS ticker; AchievedQPS in the summary is the honest measured rate)")
+	duration := fs.Duration("duration", 0, "stop after this duration (zero = run until SIGINT/SIGTERM)")
+	seed := fs.Int64("seed", time.Now().UnixNano(), "random seed for the Generator (set to a fixed value for deterministic mixes across runs)")
+	timeout := fs.Duration("timeout", 5*time.Second, "per-request HTTP timeout")
+	if err := fs.Parse(args); err != nil {
+		return err
+	}
+
+	gen, err := randommix.New(defaultBiases(), *seed)
+	if err != nil {
+		return fmt.Errorf("build generator: %w", err)
+	}
+
+	httpClient := &http.Client{Timeout: *timeout}
+	p, err := poster.New(poster.Config{
+		TargetURL: *target,
+		QPS:       *qps,
+		Duration:  *duration,
+		Client:    httpClient,
+		Out:       stderr,
+	})
+	if err != nil {
+		return fmt.Errorf("build poster: %w", err)
+	}
+
+	// stdout carries the boot-time configuration line so operators
+	// piping stdout to a structured-log aggregator capture what was
+	// requested; stderr carries the poster's per-run summary so it
+	// stays out of the structured-log stream when stdout is captured.
+	fmt.Fprintf(stdout, "traffic-gen: target=%s qps=%d duration=%s seed=%d\n",
+		*target, *qps, *duration, *seed)
+	return p.Run(ctx, gen)
+}
+
+// defaultBiases is the v0.0.1 shipped persona mix. The proportions
+// roughly mirror markup-svc's testdata/rules.csv -- some Requests
+// match the enterprise rule, some the br_peak AND-condition, some
+// the default_consumer rule, and a meaningful slice match nothing
+// (404 from markup-svc). The 404 slice is intentional: it exercises
+// the no-match path, which is part of the production wire shape.
+//
+// Operators wanting a different mix edit this function and rebuild,
+// or supply a custom Generator via a wrapper main. A config-file
+// route is a follow-up if a real operator asks for it.
+func defaultBiases() []randommix.Bias {
+	return []randommix.Bias{
+		{
+			Field: "customer_tier",
+			Values: []randommix.WeightedValue{
+				{Value: "enterprise", Weight: 20},
+				{Value: "gold", Weight: 30},
+				{Value: "silver", Weight: 30},
+				{Value: "consumer", Weight: 20},
+			},
+		},
+		{
+			Field: "country",
+			Values: []randommix.WeightedValue{
+				{Value: "BR", Weight: 30},
+				{Value: "DE", Weight: 25},
+				{Value: "FR", Weight: 20},
+				{Value: "NL", Weight: 10},
+				{Value: "ES", Weight: 10},
+				{Value: "IT", Weight: 5},
+			},
+		},
+		{
+			Field: "time_window",
+			Values: []randommix.WeightedValue{
+				{Value: "peak", Weight: 30},
+				{Value: "off", Weight: 40},
+				{Value: "normal", Weight: 30},
+			},
+		},
+		{
+			Field: "channel",
+			Values: []randommix.WeightedValue{
+				{Value: "web", Weight: 60},
+				{Value: "app", Weight: 30},
+				{Value: "partner", Weight: 10},
+			},
+		},
+	}
+}
+
+// Compile-time assertion the wired generator satisfies the port.
+var _ traffic.Generator = (*randommix.Generator)(nil)
+var _ traffic.Poster = (*poster.Poster)(nil)
---
+++ cmd/traffic-gen/main_test.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/helmedeiros/traffic-gen/internal/traffic"
+)
+
+// TestRunPostsAgainstTargetWithDuration is the integration smoke test
+// for the cmd binary. Spin up an httptest server, point the binary
+// at it with --duration so the run terminates cleanly, and confirm
+// the server saw at least a handful of POSTs.
+func TestRunPostsAgainstTargetWithDuration(t *testing.T) {
+	var hits int64
+	var sawContentType atomic.Bool
+	var sawValidRequest atomic.Bool
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		atomic.AddInt64(&hits, 1)
+		if r.Header.Get("Content-Type") == "application/json" {
+			sawContentType.Store(true)
+		}
+		var body traffic.Request
+		if err := json.NewDecoder(r.Body).Decode(&body); err == nil {
+			sawValidRequest.Store(true)
+		}
+		w.WriteHeader(http.StatusOK)
+	}))
+	t.Cleanup(srv.Close)
+
+	var stdout, stderr bytes.Buffer
+	args := []string{
+		"--target", srv.URL,
+		"--qps", "100",
+		"--duration", "80ms",
+		"--seed", "1",
+	}
+	if err := run(context.Background(), args, &stdout, &stderr); err != nil {
+		t.Fatalf("run: %v", err)
+	}
+	if got := atomic.LoadInt64(&hits); got < 3 {
+		t.Errorf("server saw %d POSTs; want >= 3 (logs: stderr=%q)", got, stderr.String())
+	}
+	if !sawContentType.Load() {
+		t.Error("server never saw Content-Type: application/json on a POST")
+	}
+	if !sawValidRequest.Load() {
+		t.Error("server never decoded a POST body as a traffic.Request")
+	}
+	if !strings.Contains(stdout.String(), "target=") {
+		t.Errorf("stdout missing boot line: %q", stdout.String())
+	}
+	if !strings.Contains(stderr.String(), "poster: done") {
+		t.Errorf("stderr missing poster summary: %q", stderr.String())
+	}
+}
+
+func TestRunReturnsErrorOnBadFlag(t *testing.T) {
+	var stdout, stderr bytes.Buffer
+	err := run(context.Background(), []string{"--qps", "not-a-number"}, &stdout, &stderr)
+	if err == nil {
+		t.Fatal("run accepted bad --qps; want error")
+	}
+}
+
+func TestRunReturnsErrorOnNegativeQPS(t *testing.T) {
+	var stdout, stderr bytes.Buffer
+	err := run(context.Background(), []string{"--qps", "-1", "--target", "http://example/decide"}, &stdout, &stderr)
+	if err == nil {
+		t.Fatal("run accepted --qps=-1; want error")
+	}
+}
+
+// TestRunStopsOnContextCancel covers the SIGINT/SIGTERM path the
+// real main() uses: a ctx cancel during a long run returns nil
+// without crashing.
+func TestRunStopsOnContextCancel(t *testing.T) {
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+	t.Cleanup(srv.Close)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		time.Sleep(40 * time.Millisecond)
+		cancel()
+	}()
+
+	var stdout, stderr bytes.Buffer
+	args := []string{"--target", srv.URL, "--qps", "50"}
+	start := time.Now()
+	if err := run(ctx, args, &stdout, &stderr); err != nil {
+		t.Fatalf("run: %v", err)
+	}
+	if time.Since(start) > 300*time.Millisecond {
+		t.Errorf("run took %s after ctx canceled at 40ms; want <300ms", time.Since(start))
+	}
+}
